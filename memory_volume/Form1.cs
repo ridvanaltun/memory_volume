5e71b8e6a222765fe1c4baa9963e2ea68a7188a3
ridvan altun <ridvanaltun@outlook.com>
2017-01-07 03:57:44 +0300

    added some functions

--- memory_volume/Form1.cs
+++ memory_volume/Form1.cs
@@ -23,6 +23,9 @@ namespace memory_volume
 
         Process[] prc = Process.GetProcessesByName("explorer");
         int deger = 0; Int64 anaAdres = 0;
+        int offset1, offset2, offset3, offset4, offset5, offset6, yanDeger;
+
+
         private void Form1_Load(object sender, EventArgs e)
         {
             //oku = new Bellek("explorer.exe");
@@ -34,17 +37,78 @@ namespace memory_volume
                     anaAdres = modul.BaseAddress.ToInt64();
                 }
             }
-            timer1.Enabled = true;
+            
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            deger = oku.Int_OKU_Offset(anaAdres + 0xE94D0, new int[] { 0x338, 0x158, 0x40, 0xc8, 0xd8, 0xe8 });
+            deger = oku.Int_OKU_Offset(anaAdres + yanDeger, new int[] { offset1, offset2, offset3, offset4, offset5, offset6 });
             if (deger < 101)
             {
                 progressBar1.Value = deger;
                 this.Text = "%" + deger;
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(button1.Text == "Apply")
+            {
+
+                if (textBox1.Text == "0x" || textBox4.Text == "0x" || textBox11.Text == "0x" || textBox8.Text == "0x" || textBox7.Text == "0x" || textBox6.Text == "0x" || textBox5.Text == "0x")
+                {
+                    label1.Text = "0x is bad";
+                }
+                else
+                {
+                    label1.Text = "good job";
+                    yanDeger = Convert.ToInt32(textBox1.Text);
+                    offset1 = Convert.ToInt32(textBox4.Text);
+                    offset2 = Convert.ToInt32(textBox11.Text);
+                    offset3 = Convert.ToInt32(textBox8.Text);
+                    offset4 = Convert.ToInt32(textBox7.Text);
+                    offset5 = Convert.ToInt32(textBox6.Text);
+                    offset6 = Convert.ToInt32(textBox5.Text);
+
+                    textBox1.Enabled = false;
+                    textBox4.Enabled = false;
+                    textBox11.Enabled = false;
+                    textBox8.Enabled = false;
+                    textBox7.Enabled = false;
+                    textBox6.Enabled = false;
+                    textBox5.Enabled = false;
+
+                    button1.Text = "Stop";
+                    button2.Enabled = false;
+                    timer1.Enabled = true;
+                }
+
+            }
+            else
+            {
+                button1.Text = "Apply";
+                button2.Enabled = true;
+                timer1.Enabled = false;
+
+                textBox1.Enabled = true;
+                textBox4.Enabled = true;
+                textBox11.Enabled = true;
+                textBox8.Enabled = true;
+                textBox7.Enabled = true;
+                textBox6.Enabled = true;
+                textBox5.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0xE94D0";
+            textBox4.Text = "0x";
+            textBox11.Text = "0x";
+            textBox8.Text = "0x";
+            textBox7.Text = "0x";
+            textBox6.Text = "0x";
+            textBox5.Text = "0x";
         }
     }
 }
